f990cc370f7348cf9bb75757bc8e58d6cb87e945
Vincent DARON <vda@dfakto.com>
2021-02-26 15:20:38 +0100

    Add sample console host

---
+++ dFakto.AppDataPathSampleHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using dFakto.AppDataPath;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace dFakto.AppDataPathSampleHost
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .AddAppData("section")
+                .ConfigureServices((_, services) =>
+                    services.AddHostedService<RootHostedService>());
+    }
+}
