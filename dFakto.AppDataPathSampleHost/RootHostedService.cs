7bcd692dbaadfaa969dfb95eb49f8f1bdbe4de12
Vincent DARON <vda@dfakto.com>
2021-02-26 15:01:36 +0100

    Add sample console host

---
+++ dFakto.AppDataPathSampleHost/RootHostedService.cs
@@ -0,0 +1,52 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace dFakto.AppDataPathSampleHost
+{
+    public class RootHostedService : IHostedService
+    {
+        private readonly ILogger _logger;
+        
+        public RootHostedService(
+            ILogger<RootHostedService> logger,
+            IHostApplicationLifetime appLifetime)
+        {
+            _logger = logger;
+
+            appLifetime.ApplicationStarted.Register(OnStarted);
+            appLifetime.ApplicationStopping.Register(OnStopping);
+            appLifetime.ApplicationStopped.Register(OnStopped);
+        }
+        
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("1. StartAsync has been called");
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("4. StopAsync has been called");
+
+            return Task.CompletedTask;
+        }
+
+        private void OnStarted()
+        {
+            _logger.LogInformation("2. OnStarted has been called");
+        }
+
+        private void OnStopping()
+        {
+            _logger.LogInformation("3. OnStopping has been called");
+        }
+
+        private void OnStopped()
+        {
+            _logger.LogInformation("5. OnStopped has been called");
+        }
+    }
+}
